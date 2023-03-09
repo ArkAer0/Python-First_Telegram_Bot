7c30a84241c2956bb40525e1089d5d03b093da1e
AlterEg0 <brotan555@mail.ru>
2023-03-09 04:12:50 +0300

    Repeat

--- handlers/other.py
+++ handlers/other.py
@@ -1,7 +1,10 @@
-from aiogram import types
+from aiogram import types, Dispatcher
 from create_bot import dp
 
 # Хэндлер на команду /test1
-@dp.message_handler(text=['Люблю','люблю','коть'])
+#@dp.message_handler(text=['Люблю','люблю','коть'])
 async def cmd_test2(message: types.Message):
     await message.reply("И я тебя очень люблю, лапочка")
+
+def handlers_other(dp: Dispatcher):
+    dp.register_message_handler(cmd_test2, text=['Люблю','люблю','коть'])
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+#Здесь происходит инициализация всего бота работает как точка входа
 from aiogram import executor
 from create_bot import dp
+
+from handlers import clients, admin, other
+
+clients.handlers_clients(dp)
+other.handlers_other(dp)
 
 if __name__ == "__main__":
     # Запуск бота
